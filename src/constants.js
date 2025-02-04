ed88321becddb7738b21f4f07158a9d4357dadd7
Rehman Asif <muhammadrehman8164@gmail.com>
2025-02-04 11:19:42 -0800

    MonogDB connection

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+const DB_NAME="video tube"
+
+export {DB_NAME}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('helloion')
+import dotenv from "dotenv"
+
+import connectDB from "./db/database_connection.js";
+
+dotenv.config(
+    {
+        path:"./env"
+    }
+)
+
+connectDB()
